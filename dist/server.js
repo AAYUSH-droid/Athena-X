a15553cdf853d272f30fa9caf472b9aa1ab8efb2
aayush <sharma31aayush@gmail.com>
2023-08-24 20:51:44 +0530

    fix

--- dist/server.js
+++ dist/server.js
@@ -7,7 +7,7 @@ const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
 const port = 3000;
 app.get("/", (req, res) => {
-    res.send("Hello, this is Express + TypeScript");
+    res.send("Hello, this is Express + TypeScript/");
 });
 app.listen(port, () => {
     console.log(`[Server]: I am running at http://localhost:${port}`);
--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,7 @@ const app: Express = express();
 const port = 3000;
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Hello, this is Express + TypeScript");
+  res.send("Hello, this is Express + TypeScript/");
 });
 
 app.listen(port, () => {
