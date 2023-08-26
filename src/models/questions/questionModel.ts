22e7cdffa05d56cb49fc81231bf6ffbc8c96af83
aayush <sharma31aayush@gmail.com>
2023-08-26 16:02:56 +0530

    FEAT: quiz, question Models Defined

---
+++ src/models/questions/questionModel.ts
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+import { ModelNames, IQuestion } from "types";
+import questionSchema from "./questionSchema";
+
+const QuestionModel = mongoose.model<IQuestion>(
+  ModelNames.Question,
+  questionSchema
+);
+
+export default QuestionModel;
